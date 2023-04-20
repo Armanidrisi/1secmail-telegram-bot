4105e9f866a3ad3265c83bc2b6a49f1b17440eb1
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2023-04-21 00:03:25 +0530

    Update Database.py

--- Database.py
+++ Database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'users' # set the database table name as users
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
@@ -13,10 +13,12 @@ class User(Base):
     email = Column(String)
 
 class UserDatabase:
+  #create database connection on constructor
     def __init__(self, database_url):
         self.engine = create_engine(database_url)
         self.Session = sessionmaker(bind=self.engine)
 
+     # function for insert data
     def create_user(self, first_name, last_name, chat_id, email):
         session = self.Session()
         user = User(first_name=first_name, last_name=last_name, chat_id=chat_id, email=email)
@@ -24,12 +26,14 @@ class UserDatabase:
         session.commit()
         session.refresh(user)
         return user
-
+    
+    #function for get data
     def get_user(self, user_id):
         session = self.Session()
         user = session.query(User).filter_by(chat_id=user_id).first()
         return user
 
+     # function for update data
     def update_user(self, user_id, new_data):
         session = self.Session()
         user = session.query(User).filter_by(chat_id=user_id).first()
@@ -39,12 +43,14 @@ class UserDatabase:
             setattr(user, key, value)
         session.commit()
         return True
-
     
+    #function for get num rows by id
     def get_num_rows_by_id(self, chat_id):
         session = self.Session()
         num_rows = session.query(User).filter_by(chat_id=chat_id).count()
         return num_rows
+  
+  # function for get all num rows
     def get_num_rows(self):
       session=self.Session()
       num_rows=session.query(User).count()
