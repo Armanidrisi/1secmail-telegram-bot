73014653ed284e79c01942fe6b43d7d97f80897a
Armanidrisi <idrisiarman17@gmail.com>
2023-04-21 12:04:09 +0530

    Added Some Database Examples

--- Config.py
+++ Config.py
@@ -1,3 +1,8 @@
 DATABASE_URI ="sqlite:///database.db" #Database URI
+"""examples of uri for databases
+microsoft sql server: mssql+pyodbc://<username>:<password>@<dsn_name>
+mysql: mysql://<username>:<password>@<hostname>:<port>/<database_name>
+postgresql: postgresql://<username>:<password>@<hostname>:<port>/<database_name>"""
+
 TOKEN = "ADD YOU BOT TOKEN" #Bot Token 
-BASE_URL ="https://www.1secmail.com/api/v1/" #API Base URL
+BASE_URL ="https://www.1secmail.com/api/v1/" #API Base URL.!Don't Change 
