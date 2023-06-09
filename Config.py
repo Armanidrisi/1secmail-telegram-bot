907c89a8e496d7a4f6d036204ef2c02502ed8611
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2023-06-09 14:27:53 +0530

    added bot token for deployment..

--- Config.py
+++ Config.py
@@ -1,8 +1,11 @@
-DATABASE_URI ="sqlite:///database.db" #Database URI
-"""examples of uri for databases
-microsoft sql server: mssql+pyodbc://<username>:<password>@<dsn_name>
-mysql: mysql://<username>:<password>@<hostname>:<port>/<database_name>
-postgresql: postgresql://<username>:<password>@<hostname>:<port>/<database_name>"""
+DATABASE_URI = "sqlite:///database.db"  # Database URI
+"""
+Example URIs for different database types:
+- Microsoft SQL Server: mssql+pyodbc://<username>:<password>@<dsn_name>
+- MySQL: mysql://<username>:<password>@<hostname>:<port>/<database_name>
+- PostgreSQL: postgresql://<username>:<password>@<hostname>:<port>/<database_name>
+"""
 
-TOKEN = "ADD YOU BOT TOKEN" #Bot Token 
-BASE_URL ="https://www.1secmail.com/api/v1/" #API Base URL.!Don't Change 
+TOKEN = "5911287237:AAEp48qWCK2UxtL-Q6mrfRVYxf6ZjBJUS1w"  # Bot Token
+
+BASE_URL = "https://www.1secmail.com/api/v1/"  # API Base URL. Do not change
