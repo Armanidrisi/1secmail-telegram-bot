b5945e3ee553bde081064e29e1c840e488b6bced
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2023-12-26 20:02:06 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,6 +6,6 @@ Example URIs for different database types:
 - PostgreSQL: postgresql://<username>:<password>@<hostname>:<port>/<database_name>
 """
 
-TOKEN = "5911287237:AAEp48qWCK2UxtL-Q6mrfRVYxf6ZjBJUS1w"  # Bot Token
+TOKEN = "6633482629:AAF-ifKANp5oD3RIKAtGrMdV8_sf-2EwvIQ"  # Bot Token
 
 BASE_URL = "https://www.1secmail.com/api/v1/"  # API Base URL. Do not change
